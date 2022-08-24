afbf85c4b85249c06b6640937c8081f86dfffaf6
tuliomitico <tulioneedforspeed@gmail.com>
2022-08-24 19:14:44 -0300

    chore: add graceful shutdown, improve logging

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from src.server import create_app
+import signal
+import sys
+import logging
+from enum import Enum
 
-app = create_app('src.config.ProductionConfig')
+from src.server import create_app
+import src.logger
+
+app = create_app('src.config.DevelopmentConfig')
+
+class ExitStatus(Enum):
+    Failure = 1
+    Success = 0
+
+def handler(signum, frame):
+    logging.info('App exited with success')
+    sys.exit(ExitStatus.Success)
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0",load_dotenv=True)
+    try:
+        EXIT_SIGNALS: "list[signal.Signals]" = [signal.SIGINT,signal.SIGTERM]
+        for sig in EXIT_SIGNALS:
+            try:
+                signal.signal(sig,handler)
+            except Exception as error:
+                logging.error(f"App exited with error: {repr(error)}")  
+                sys.exit(ExitStatus.Failure)
+        app.run(host = "0.0.0.0",load_dotenv=True)
+
+    except Exception as error:
+        logging.error(f"App exited with error: {repr(error)}")
+        sys.exit(ExitStatus.Failure)
--- src/logger.py
+++ src/logger.py
@@ -1,3 +1,8 @@
 import logging
+
+logging.basicConfig(
+    level=logging.INFO, 
+    format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s'
+)
 logger = logging.getLogger('waitress')
 logger.setLevel(logging.INFO)

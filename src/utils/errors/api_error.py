16310e44e5d358b143d6367c4c515f50e6c895b1
tuliomitico <tulioneedforspeed@gmail.com>
2022-11-23 21:15:28 -0300

    feat: add new error functionality

---
+++ src/utils/errors/api_error.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass, field
+from http import HTTPStatus
+from numbers import Number
+from typing import Union
+import http.client
+
+@dataclass
+class APIError():
+    message: str
+    code: Number
+    code_as_string: Union[str,None] = field(default=None)
+    description: Union[str,None] = field(default=None)
+    documentation: Union[str,None] = field(default=None)
+
+@dataclass
+class APIErrorResponse(APIError):
+    error: str = field(default_factory=str)
+
+class ApiError():
+    @staticmethod
+    def format(error: APIError) -> APIErrorResponse:
+        if error.description:
+            return {
+                "message": error.message,
+                "code": error.code,
+                "error": error.code_as_string if error.code_as_string else http.client.responses[error.code],
+                "description": error.description,
+            }
+        if error.documentation:
+            return {
+                "message": error.message,
+                "code": error.code,
+                "error": error.code_as_string if error.code_as_string else http.client.responses[error.code],
+                "description": error.description,
+                "documentation": error.documentation,
+        }
+        return {
+            "message": error.message,
+            "code": error.code,
+            "error": error.code_as_string if error.code_as_string else http.client.responses[error.code],
+        }

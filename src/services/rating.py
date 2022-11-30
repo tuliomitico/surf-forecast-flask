673363a6c2bcb045e5adb93aca39f0c352df5e7f
tuliomitico <tulioneedforspeed@gmail.com>
2022-11-30 19:15:06 -0300

    feat: add the rating logic

---
+++ src/services/rating.py
@@ -0,0 +1,83 @@
+from typing import TypedDict
+
+from ..models.beach import Beach
+
+class Height(TypedDict):
+    minimum: float
+    maximum: float 
+
+class WaveHeights(TypedDict):
+    ankle_to_knee: Height
+    waist_high: Height
+    overhead_high: Height
+
+# meters
+wave_heights: WaveHeights = {
+    "ankle_to_knee": {
+        "minimum": 0.3,
+        "maximum": 1.0
+    },
+    "waist_high": {
+        "minimum": 1.0,
+        "maximum": 2.0
+    },
+    "overhead_high": {
+        "minimum": 2.0,
+        "maximum": 2.5
+    }
+}
+
+class Rating():
+
+    def __init__(self, beach: Beach) -> None:
+        self.__beach = beach
+
+    def get_rating_based_on_wind_and_wave_positions(
+        self,wave_position: str,wind_position: str
+    ) -> int:
+        if wave_position == wind_position:
+            return 1
+        elif self.__is_wind_offshore(wave_position,wind_position):
+            return 5
+        return 3
+    
+    def get_rating_for_swell_period(
+        self, period: int
+    ) -> int:
+        if (period < 7):
+            return 1
+        if (period < 10): 
+            return 2
+        if (period < 14): 
+            return 4;
+        return 5;
+    
+    def get_rating_for_swell_size(
+        self,height: float
+    ) -> int:
+        if (height < wave_heights["ankle_to_knee"]["minimum"]):
+            return 1
+        if (height < wave_heights['ankle_to_knee']['maximum']): 
+            return 2
+        if (height < wave_heights["waist_high"]["maximum"]): 
+            return 3
+        return 5
+
+    def get_position_from_location(self, coordinates: float) -> str:
+        if (coordinates < 50): 
+            return "N"
+        if (coordinates < 120): 
+            return "E"
+        if (coordinates < 220): 
+            return "S"
+        if (coordinates < 310): 
+            return "W"
+        return "N"
+
+    def __is_wind_offshore(self,wave_position: str,wind_position: str) -> bool:
+        return (
+            (wave_position == "N" and wind_position == "S" and self.__beach.position=="N") or
+            (wave_position == "S" and wind_position == "N" and self.__beach.position=="S") or
+            (wave_position == "E" and wind_position == "W" and self.__beach.position=="E") or
+            (wave_position == "W" and wind_position == "E" and self.__beach.position=="W") 
+            )
---
+++ src/services/tests/test_rating.py
@@ -0,0 +1,111 @@
+"""Ratings service"""
+import pytest
+
+
+from ..rating import Rating
+from ...models.beach import Beach
+
+pytestmark = pytest.mark.unit
+
+beach = {
+    "lat": -33.792726,
+    "lng": 151.289824,
+    "name": "Manly",
+    "position": "E",
+}
+
+default_rating = Rating(Beach(**beach))
+
+@pytest.mark.skip()
+def test_padrao():
+    """Calculate rate for a given point."""
+    # TODO
+    assert True
+
+@pytest.mark.skip()    
+def test_padrao_2():
+    """Get rating based on wind and wave positions."""
+
+def test_onshore_winds():
+    """Get rating 1 for a beach with onshore winds."""
+    rating = default_rating.get_rating_based_on_wind_and_wave_positions("E","E")
+    assert rating == 1
+
+def test_cross_winds():
+    """Get rating 3 for a beach with cross winds."""
+    rating = default_rating.get_rating_based_on_wind_and_wave_positions("E","S")
+    assert rating == 3
+
+def test_offshore_winds():
+    """Get rating 5 for a beach with offshore winds."""
+    rating = default_rating.get_rating_based_on_wind_and_wave_positions("E","W")
+    assert rating == 5
+
+# Get rating based on swell period
+def test_rating_period_of_5():
+    """Should get a rating of 1 for a period of 5 seconds"""
+    rating = default_rating.get_rating_for_swell_period(5)
+    assert rating == 1
+
+def test_rating_period_of_9():
+    """Should get a rating of 2 for a period of 9 seconds"""
+    rating = default_rating.get_rating_for_swell_period(9)
+    assert rating == 2
+
+def test_rating_period_of_12():
+    """Should get a rating of 4 for a period of 12 seconds"""
+    rating = default_rating.get_rating_for_swell_period(12)
+    assert rating == 4
+    
+def test_rating_period_of_16():
+    """Should get a rating of 5 for a period of 16 seconds"""
+    rating = default_rating.get_rating_for_swell_period(16)
+    assert rating == 5
+
+# Get rating based on swell height
+def test_rating_swell_size_less_than_ankle_to_knee():
+    """Should get a rating of 1 for less than a ankle to knee high sweel"""
+    rating = default_rating.get_rating_for_swell_size(0.2)
+    assert rating == 1
+
+def test_rating_swell_size_of_ankle_to_knee():
+    """Should get a rating of 2 for an ankle to knee high sweel"""
+    rating = default_rating.get_rating_for_swell_size(0.6)
+    assert rating == 2
+
+def test_rating_sweel_size_of_waist():
+    """Should get a rating of 3 for a waist high sweel"""
+    rating = default_rating.get_rating_for_swell_size(1.5)
+    assert rating == 3
+
+def test_rating_swell_size_of_overhead():
+    """Should get a rating of 5 for a overhead high sweel"""
+    rating = default_rating.get_rating_for_swell_size(2)
+    assert rating == 5
+
+# Get position based on points location
+def test_position_east():
+    """Should get the point based on the east location"""
+    rating = default_rating.get_position_from_location(92)
+    assert rating == "E"
+
+def test_position_north_1():
+    """Should get the point based on the north location 1"""
+    rating = default_rating.get_position_from_location(360)
+    assert rating == "N"
+
+def test_position_north_2():
+    """Should get the point based on the north location 2"""
+    rating = default_rating.get_position_from_location(40)
+    assert rating == "N"
+
+def test_position_south():
+    """Should get the point based on the south location"""
+    rating = default_rating.get_position_from_location(200)
+    assert rating == "S"
+
+def test_position_west():
+    """Should get the point based on the west location"""
+    rating = default_rating.get_position_from_location(300)
+    assert rating == "W"
+ 
